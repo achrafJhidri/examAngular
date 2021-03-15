d341ae7b1640e85c71cc472559e0431ef05598be
achraf jhidri <achrafjh@gmail.com>
2021-03-15 16:16:50 +0100

    search filter

---
+++ src/app/shared/pipes/filters/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Element } from '../../model/element.model';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(elements: Element[], search: string ): Element[]  {
+    if (!search.length) {
+       return elements;
+    }
+    else {
+      return elements.filter((element: Element) =>
+      element.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  }
+
+}
