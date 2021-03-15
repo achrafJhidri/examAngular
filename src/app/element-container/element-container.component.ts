40dcceee5b5d0a10cd35aad42ee406cc0e83c929
achraf jhidri <achrafjh@gmail.com>
2021-03-15 16:15:09 +0100

    element container component

--- src/app/element-container/element-container.component.ts
+++ src/app/element-container/element-container.component.ts
@@ -1,1 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ElementService } from '../shared/service/element.service';
+@Component({
+  selector: 'app-element-container',
+  templateUrl: './element-container.component.html',
+  styleUrls: ['./element-container.component.css'],
   providers:[ ElementService]
+})
+export class ElementContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
